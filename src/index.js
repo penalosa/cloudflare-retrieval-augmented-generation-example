f7963cb2c6440cced12de7c686d61014f0b57e35
Samuel Macleod <smacleod@cloudflare.com>
2023-11-27 15:31:01 -0800

    Additions

--- src/index.js
+++ src/index.js
@@ -1,92 +1,91 @@
-import { Ai } from "@cloudflare/ai";
-import { Hono } from 'hono'
+import { Ai } from '@cloudflare/ai';
+import { Hono } from 'hono';
 
-import ui from './ui.html'
-import write from './write.html'
+import ui from './ui.html';
+import write from './write.html';
+import notes from './notes.html';
 
-const app = new Hono()
+const app = new Hono();
 
 app.post('/notes', async (c) => {
-  const ai = new Ai(c.env.AI)
+	const ai = new Ai(c.env.AI);
 
-  const { text } = await c.req.json();
-  if (!text) c.throw(400, "Missing text");
+	const { text } = await c.req.json();
+	if (!text) c.throw(400, 'Missing text');
 
-  const { results } = await c.env.DATABASE.prepare("INSERT INTO notes (text) VALUES (?) RETURNING *")
-    .bind(text)
-    .run()
+	const { results } = await c.env.DATABASE.prepare('INSERT INTO notes (text) VALUES (?) RETURNING *').bind(text).run();
 
-  const record = results.length ? results[0] : null
+	const record = results.length ? results[0] : null;
 
-  if (!record) c.throw(500, "Failed to create note")
+	if (!record) c.throw(500, 'Failed to create note');
 
-  const { data } = await ai.run('@cf/baai/bge-base-en-v1.5', { text: [text] })
-  const values = data[0]
+	const { data } = await ai.run('@cf/baai/bge-base-en-v1.5', { text: [text] });
+	const values = data[0];
 
-  if (!values) c.throw(500, "Failed to generate vector embedding")
+	if (!values) c.throw(500, 'Failed to generate vector embedding');
 
-  const { id } = record
-  const inserted = await c.env.VECTOR_INDEX.upsert([
-    {
-      id: id.toString(),
-      values,
-    }
-  ]);
+	const { id } = record;
+	const inserted = await c.env.VECTOR_INDEX.upsert([
+		{
+			id: id.toString(),
+			values,
+		},
+	]);
 
-  return c.json({ id, text, inserted });
-})
+	return c.json({ id, text, inserted });
+});
 
 app.get('/ui', async (c) => {
 	return c.html(ui);
-})
+});
 
 app.get('/write', async (c) => {
 	return c.html(write);
-})
+});
+
+app.get('/notes', async (c) => {
+	const query = `SELECT * FROM notes`;
+	const { results } = await c.env.DATABASE.prepare(query).bind().all();
+	console.log(results[0]);
+	return c.html(notes.replace('{LIST_PLACEHOLDER}', results.map((r) => `<li>${r.text}</li>`).join('')));
+});
 
 app.get('/', async (c) => {
-  const ai = new Ai(c.env.AI);
+	const ai = new Ai(c.env.AI);
 
-  const question = c.req.query('text') || "What is the square root of 9?"
+	const question = c.req.query('text') || 'What is the square root of 9?';
 
-  const embeddings = await ai.run('@cf/baai/bge-base-en-v1.5', { text: question })
-  const vectors = embeddings.data[0]
+	const embeddings = await ai.run('@cf/baai/bge-base-en-v1.5', { text: question });
+	const vectors = embeddings.data[0];
 
-  const SIMILARITY_CUTOFF = 0.75
-  const vectorQuery = await c.env.VECTOR_INDEX.query(vectors, { topK: 1 });
-  const vecIds = vectorQuery.matches
-    .filter(vec => vec.score > SIMILARITY_CUTOFF)
-    .map(vec => vec.vectorId)
+	const SIMILARITY_CUTOFF = 0.75;
+	const vectorQuery = await c.env.VECTOR_INDEX.query(vectors, { topK: 1 });
+	const vecIds = vectorQuery.matches.filter((vec) => vec.score > SIMILARITY_CUTOFF).map((vec) => vec.vectorId);
 
-  let notes = []
-  if (vecIds.length) {
-    const query = `SELECT * FROM notes WHERE id IN (${vecIds.join(", ")})`
-    const { results } = await c.env.DATABASE.prepare(query).bind().all()
-    if (results) notes = results.map(vec => vec.text)
-  }
+	let notes = [];
+	if (vecIds.length) {
+		const query = `SELECT * FROM notes WHERE id IN (${vecIds.join(', ')})`;
+		const { results } = await c.env.DATABASE.prepare(query).bind().all();
+		if (results) notes = results.map((vec) => vec.text);
+	}
 
-  const contextMessage = notes.length
-    ? `Context:\n${notes.map(note => `- ${note}`).join("\n")}`
-    : ""
+	const contextMessage = notes.length ? `Context:\n${notes.map((note) => `- ${note}`).join('\n')}` : '';
 
-  const systemPrompt = `When answering the question or responding, use the context provided, if it is provided and relevant.`
+	const systemPrompt = `When answering the question or responding, use the context provided, if it is provided and relevant.`;
 
-  const { response: answer } = await ai.run(
-    '@cf/meta/llama-2-7b-chat-int8',
-    {
-      inputs: [
-        ...(notes.length ? [{ role: 'system', content: contextMessage }] : []),
-        { role: 'system', content: systemPrompt },
-        { role: 'user', content: question }
-      ]
-    }
-  )
+	const { response: answer } = await ai.run('@cf/meta/llama-2-7b-chat-int8', {
+		messages: [
+			...(notes.length ? [{ role: 'system', content: contextMessage }] : []),
+			{ role: 'system', content: systemPrompt },
+			{ role: 'user', content: question },
+		],
+	});
 
-  return c.text(answer);
-})
+	return c.text(answer);
+});
 
 app.onError((err, c) => {
-  return c.text(err)
-})
+	return c.text(err);
+});
 
-export default app
+export default app;
